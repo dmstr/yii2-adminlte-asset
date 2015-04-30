d49ebcc47536c80b12f9e21e01e419d1206d686b
Evgeniy Tkachenko <e.jeka.mail@gmail.com>
2015-04-30 15:21:03 +0300

    Update login.php

--- example-views/yiisoft/yii2-app/site/login.php
+++ example-views/yiisoft/yii2-app/site/login.php
@@ -8,8 +8,15 @@ use yii\bootstrap\ActiveForm;
 
 $this->title = 'Sign In';
 
-$fieldOptions1 = ['options' => ['class' => 'form-group has-feedback'], 'inputTemplate' => "{input}<span class='glyphicon glyphicon-envelope form-control-feedback'></span>"];
-$fieldOptions2 = ['options' => ['class' => 'form-group has-feedback'], 'inputTemplate' => "{input}<span class='glyphicon glyphicon-lock form-control-feedback'></span>"];
+$fieldOptions1 = [
+    'options' => ['class' => 'form-group has-feedback'],
+    'inputTemplate' => "{input}<span class='glyphicon glyphicon-envelope form-control-feedback'></span>"
+];
+
+$fieldOptions2 = [
+    'options' => ['class' => 'form-group has-feedback'],
+    'inputTemplate' => "{input}<span class='glyphicon glyphicon-lock form-control-feedback'></span>"
+];
 ?>
 
 <div class="login-box">
