37be2b497a8611c2af3b56751109b1cda5ea7cd0
Evgeniy Tkachenko <et.coder@gmail.com>
2015-06-19 15:27:45 +0300

    Update main.php
    Add remark
    
    P.S. special for susisi

--- example-views/yiisoft/yii2-app/layouts/main.php
+++ example-views/yiisoft/yii2-app/layouts/main.php
@@ -3,7 +3,13 @@ use yii\helpers\Html;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
-if (Yii::$app->controller->action->id === 'login') {
+
+
+if (Yii::$app->controller->action->id === 'login') { 
+/**
+ * Do not use this code in your template. Remove it. 
+ * Instead, use the code  $this->layout = '//main-login'; in your controller.
+ */
     echo $this->render(
         'main-login',
         ['content' => $content]
