eb502e72aa2a6d7f0775ee001a95c2f191c1cc3f
Evgeniy Tkachenko <et.coder@gmail.com>
2015-06-19 15:04:27 +0300

    Update AdminLteHelper.php
    Added phpdoc block.

--- helpers/AdminLteHelper.php
+++ helpers/AdminLteHelper.php
@@ -6,6 +6,11 @@ use Yii;
 class AdminLteHelper
 {
     /**
+     * It allows you to get the name of the css class.
+     * You can add the appropriate class to the body tag for dynamic change the template's appearance.
+     * Note: Use this fucntion only if you override the skin through configuration. 
+     * Otherwise you will not get the correct css class of body.
+     * 
      * @return string
      */
     public static function skinClass()
