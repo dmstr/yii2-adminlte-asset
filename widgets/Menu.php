2e94f6dea3d1b74df9748f53aebbf95e4a81f6f7
Vadim Mazur <mazurva@gmail.com>
2015-08-07 12:17:45 +0600

    Add comments

--- widgets/Menu.php
+++ widgets/Menu.php
@@ -1,19 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vadim
- * Date: 07.08.15
- * Time: 11:40
- */
 
 namespace dmstr\widgets;
 
 use yii;
 
+/**
+ * Class Menu
+ * @package dmstr\widgets *
+ * @author Vadim Mazur <mazurva@gmail.com>, Qiang Xue <qiang.xue@gmail.com>
+ */
 class Menu extends yii\widgets\Menu
 {
+    /**
+     * @var string the template used to render a list of sub-menus.
+     * In this template, the token `{items}` will be replaced with the rendered sub-menu items.
+     */
     public $submenuTemplate = '<ul class="treeview-menu">{items}</ul>';
 
+    /**
+     * Renders the content of a menu item.
+     * Note that the container and the sub-menus are not rendered here.
+     * @param array $item the menu item to be rendered. Please refer to [[items]] to see what data might be in the item.
+     * @return string the rendering result
+     */
     protected function renderItem($item)
     {
         if(isset($item['items'])){
