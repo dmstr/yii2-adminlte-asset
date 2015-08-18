c8a1a577001445a3dd98470bfc7464a0ccaf9a03
Vadim Mazur <mazurva@gmail.com>
2015-08-18 23:59:08 +0600

    Update widget Menu.php and file _sidebar.php for support Phundament 4

--- widgets/Menu.php
+++ widgets/Menu.php
@@ -38,6 +38,31 @@ class Menu extends yii\widgets\Menu
     public $submenuTemplate = '<ul class="treeview-menu">{items}</ul>';
 
     /**
+     * @var string
+     */
+    public $countSubmenuTemplate = '<span class="label label-primary pull-right">{count}</span>';
+
+    /**
+     * @var bool
+     */
+    public $showCountSubmenu = false;
+
+    /**
+     * @var bool
+     */
+    public $showIcon = false;
+
+    /**
+     * @var string
+     */
+    public $defaultIcon = 'fa fa-circle-o';
+
+    /**
+     * @var string
+     */
+    public $iconTemplate = '<i class="{icon}"></i><span>{label}</span>';
+
+    /**
      * Renders the content of a menu item.
      * Note that the container and the sub-menus are not rendered here.
      * @param array $item the menu item to be rendered. Please refer to [[items]] to see what data might be in the item.
@@ -45,8 +70,17 @@ class Menu extends yii\widgets\Menu
      */
     protected function renderItem($item)
     {
+        if($this->showIcon&&isset($item['url'])){
+            if(!strpos($item['label'], '</i>'))
+                $item['label'] = strtr($this->iconTemplate,
+                    ['{icon}'=>(isset($item['icon'])?$item['icon']:$this->defaultIcon),
+                        '{label}'=>$item['label']]);
+        }
+
         if(isset($item['items'])){
-            $item['label'] .= '<i class="fa fa-angle-left pull-right"></i>';
+            $item['label'] .= ($this->showCountSubmenu ?
+                strtr($this->countSubmenuTemplate, ['{count}'=>count($item['items'])]):
+                '<i class="fa fa-angle-left pull-right"></i>');
         }
 
         return parent::renderItem($item);
