35972f5ede522516ef3fa9a93969a040ac255fdd
Manop Kongoon <kongoon@hotmail.com>
2015-09-05 23:03:30 +0700

    Update Menu.php
    Update new menu for AdminLTE theme to activate current route

--- widgets/Menu.php
+++ widgets/Menu.php
@@ -1,88 +1,163 @@
 <?php
-
-namespace dmstr\widgets;
-
-use yii;
-
+namespace common\widgets;
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
+use yii\helpers\Html;
 /**
  * Class Menu
- * @package dmstr\widgets *
- * @author Vadim Mazur <mazurva@gmail.com>, Qiang Xue <qiang.xue@gmail.com>
+ * Theme menu widget.
  */
-class Menu extends yii\widgets\Menu
+class Menu extends \yii\widgets\Menu
 {
     /**
-     * @var boolean whether the labels for menu items should be HTML-encoded.
+     * @inheritdoc
      */
-    public $encodeLabels = false;
-
-    /**
-     * @var boolean whether to activate parent menu items when one of the corresponding child menu items is active.
-     * The activated parent menu items will also have its CSS classes appended with [[activeCssClass]].
-     */
+    public $linkTemplate = '<a href="{url}">{icon} {label}</a>';
+    public $submenuTemplate = "\n<ul class='treeview-menu' {show}>\n{items}\n</ul>\n";
     public $activateParents = true;
-
     /**
-     * @var array the HTML attributes for the menu's container tag. The following special options are recognized:
-     *
-     * - tag: string, defaults to "ul", the tag name of the item container tags. Set to false to disable container tag.
-     *
-     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
-     */
-    public $options = ['class' => 'sidebar-menu'];
-
-    /**
-     * @var string the template used to render a list of sub-menus.
-     * In this template, the token `{items}` will be replaced with the rendered sub-menu items.
-     */
-    public $submenuTemplate = '<ul class="treeview-menu">{items}</ul>';
-
-    /**
-     * @var string
-     */
-    public $countSubmenuTemplate = '<span class="label label-primary pull-right">{count}</span>';
-
-    /**
-     * @var bool
-     */
-    public $showCountSubmenu = false;
-
-    /**
-     * @var bool
-     */
-    public $showIcon = false;
-
-    /**
-     * @var string
-     */
-    public $defaultIcon = 'fa fa-circle-o';
-
-    /**
-     * @var string
-     */
-    public $iconTemplate = '<i class="{icon}"></i><span>{label}</span>';
-
-    /**
-     * Renders the content of a menu item.
-     * Note that the container and the sub-menus are not rendered here.
-     * @param array $item the menu item to be rendered. Please refer to [[items]] to see what data might be in the item.
-     * @return string the rendering result
+     * @inheritdoc
      */
     protected function renderItem($item)
     {
-        if($this->showIcon&&isset($item['url'])){
-            if(!strpos($item['label'], '</i>'))
-                $item['label'] = strtr($this->iconTemplate,
-                    ['{icon}'=>(isset($item['icon'])?$item['icon']:$this->defaultIcon),
-                        '{label}'=>$item['label']]);
+        if(isset($item['items']))
+            $linkTemplate = '<a href="{url}">{icon} {label} <i class="fa fa-angle-left pull-right"></i></a>';
+        else
+            $linkTemplate = $this->linkTemplate;
+        if (isset($item['url'])) {
+            $template = ArrayHelper::getValue($item, 'template', $linkTemplate);
+            $replace = !empty($item['icon']) ? [
+                '{url}' => Url::to($item['url']),
+                '{label}' => '<span>'.$item['label'].'</span>',
+                '{icon}' => '<i class="fa ' . $item['icon'] . '"></i> '
+            ] : [
+                '{url}' => Url::to($item['url']),
+                '{label}' => '<span>'.$item['label'].'</span>'
+            ];
+            return strtr($template, $replace);
+        } else {
+            $template = ArrayHelper::getValue($item, 'template', $this->labelTemplate);
+            $replace = !empty($item['icon']) ? [
+                '{label}' => '<span>'.$item['label'].'</span>',
+                '{icon}' => '<i class="fa ' . $item['icon'] . '"></i> '
+            ] : [
+                '{label}' => '<span>'.$item['label'].'</span>',
+            ];
+            return strtr($template, $replace);
         }
-
-        if(isset($item['items'])){
-            $item['label'] .= ($this->showCountSubmenu ?
-                strtr($this->countSubmenuTemplate, ['{count}'=>count($item['items'])]):
-                '<i class="fa fa-angle-left pull-right"></i>');
+    }
+    /**
+     * Recursively renders the menu items (without the container tag).
+     * @param array $items the menu items to be rendered recursively
+     * @return string the rendering result
+     */
+    protected function renderItems($items)
+    {
+        $n = count($items);
+        $lines = [];
+        foreach ($items as $i => $item) {
+            $options = array_merge($this->itemOptions, ArrayHelper::getValue($item, 'options', []));
+            $tag = ArrayHelper::remove($options, 'tag', 'li');
+            $class = [];
+            if ($item['active']) {
+                $class[] = $this->activeCssClass;
+            }
+            if ($i === 0 && $this->firstItemCssClass !== null) {
+                $class[] = $this->firstItemCssClass;
+            }
+            if ($i === $n - 1 && $this->lastItemCssClass !== null) {
+                $class[] = $this->lastItemCssClass;
+            }
+            if (!empty($class)) {
+                if (empty($options['class'])) {
+                    $options['class'] = implode(' ', $class);
+                } else {
+                    $options['class'] .= ' ' . implode(' ', $class);
+                }
+            }
+            $menu = $this->renderItem($item);
+            if (!empty($item['items'])) {
+                $menu .= strtr($this->submenuTemplate, [
+                    '{show}' => $item['active'] ? "style='display: block'" : '',
+                    '{items}' => $this->renderItems($item['items']),
+                ]);
+            }
+            $lines[] = Html::tag($tag, $menu, $options);
         }
-
-        return parent::renderItem($item);
+        return implode("\n", $lines);
+    }
+    /**
+     * @inheritdoc
+     */
+    protected function normalizeItems($items, &$active)
+    {
+        foreach ($items as $i => $item) {
+            if (isset($item['visible']) && !$item['visible']) {
+                unset($items[$i]);
+                continue;
+            }
+            if (!isset($item['label'])) {
+                $item['label'] = '';
+            }
+            $encodeLabel = isset($item['encode']) ? $item['encode'] : $this->encodeLabels;
+            $items[$i]['label'] = $encodeLabel ? Html::encode($item['label']) : $item['label'];
+            $items[$i]['icon'] = isset($item['icon']) ? $item['icon'] : '';
+            $hasActiveChild = false;
+            if (isset($item['items'])) {
+                $items[$i]['items'] = $this->normalizeItems($item['items'], $hasActiveChild);
+                if (empty($items[$i]['items']) && $this->hideEmptyItems) {
+                    unset($items[$i]['items']);
+                    if (!isset($item['url'])) {
+                        unset($items[$i]);
+                        continue;
+                    }
+                }
+            }
+            if (!isset($item['active'])) {
+                if ($this->activateParents && $hasActiveChild || $this->activateItems && $this->isItemActive($item)) {
+                    $active = $items[$i]['active'] = true;
+                } else {
+                    $items[$i]['active'] = false;
+                }
+            } elseif ($item['active']) {
+                $active = true;
+            }
+        }
+        return array_values($items);
+    }
+    /**
+     * Checks whether a menu item is active.
+     * This is done by checking if [[route]] and [[params]] match that specified in the `url` option of the menu item.
+     * When the `url` option of a menu item is specified in terms of an array, its first element is treated
+     * as the route for the item and the rest of the elements are the associated parameters.
+     * Only when its route and parameters match [[route]] and [[params]], respectively, will a menu item
+     * be considered active.
+     * @param array $item the menu item to be checked
+     * @return boolean whether the menu item is active
+     */
+    protected function isItemActive($item)
+    {
+        if (isset($item['url']) && is_array($item['url']) && isset($item['url'][0])) {
+            $route = $item['url'][0];
+            if ($route[0] !== '/' && Yii::$app->controller) {
+                $route = Yii::$app->controller->module->getUniqueId() . '/' . $route;
+            }
+            $arrayRoute = explode('/', ltrim($route, '/'));
+            $arrayThisRoute = explode('/', $this->route);
+            if ($arrayRoute[0] !== $arrayThisRoute[0]) {
+                return false;
+            }
+            unset($item['url']['#']);
+            if (count($item['url']) > 1) {
+                foreach (array_splice($item['url'], 1) as $name => $value) {
+                    if ($value !== null && (!isset($this->params[$name]) || $this->params[$name] != $value)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
