d1b4ce42ab7c5f2e56c6aac4888cff2cebfaa0ee
Vasiliy Baranov <vbaranov@fbs.com>
2015-10-30 11:44:50 +0300

    Fix active menu with modules

--- widgets/Menu.php
+++ widgets/Menu.php
@@ -152,6 +152,9 @@ class Menu extends \yii\widgets\Menu
             if (isset($arrayRoute[1]) && $arrayRoute[1] !== $arrayThisRoute[1]) {
                 return false;
             }
+            if (isset($arrayRoute[2]) && $arrayRoute[2] !== $arrayThisRoute[2]) {
+                return false;
+            }
             unset($item['url']['#']);
             if (count($item['url']) > 1) {
                 foreach (array_splice($item['url'], 1) as $name => $value) {
