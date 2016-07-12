273ce3bfb291756d512c17b5ee1b47afeb39d5d5
Ilya Shashilov <kvush@mail.ru>
2016-07-12 19:38:46 +0700

    fix #88 arrow icon for multilevel item

--- widgets/Menu.php
+++ widgets/Menu.php
@@ -22,8 +22,8 @@ class Menu extends \yii\widgets\Menu
     protected function renderItem($item)
     {
         if(isset($item['items'])) {
-            $labelTemplate = '<a href="{url}">{label} <i class="fa fa-angle-left pull-right"></i></a>';
-            $linkTemplate = '<a href="{url}">{icon} {label} <i class="fa fa-angle-left pull-right"></i></a>';
+            $labelTemplate = '<a href="{url}">{label} <i class="fa fa-angle-left pull-right-container"></i></a>';
+            $linkTemplate = '<a href="{url}">{icon} {label} <i class="fa fa-angle-left pull-right-container"></i></a>';
         }
         else {
             $labelTemplate = $this->labelTemplate;
