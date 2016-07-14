ad5ab7a7fe54712e0c51c3f973491120bdb0fc8a
leonardojulius <leonardojulius.jcl@gmail.com>
2016-07-14 22:33:45 +0800

    obsevation
    temp remove profile

--- example-views/yiisoft/yii2-app/layouts/header.php
+++ example-views/yiisoft/yii2-app/layouts/header.php
@@ -24,50 +24,7 @@ use yii\helpers\Html;
                 <!-- User Account: style can be found in dropdown.less -->
 
 
-                <li class="dropdown user user-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="<?= $directoryAsset ?>/img/jjj0.jpg" class="user-image" alt="User Image"/>
-                        <span class="hidden-xs">leonardojuliusgh</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <!-- User image -->
-                        <li class="user-header">
-                            <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle"
-                                 alt="User Image"/>
-
-                            <p>
-                                Alexander Pierce - Web Developer
-                                <small>Member since Nov. 2012</small>
-                            </p>
-                        </li>
-                        <!-- Menu Body -->
-                        <li class="user-body">
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Followers</a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Sales</a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Friends</a>
-                            </div>
-                        </li>
-                        <!-- Menu Footer-->
-                        <li class="user-footer">
-                            <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
-                            </div>
-                            <div class="pull-right">
-                                <?= Html::a(
-                                    'Sign out',
-                                    ['/site/logout'],
-                                    ['data-method' => 'post', 'class' => 'btn btn-default btn-flat']
-                                ) ?>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
-
+      
                 <!-- User Account: style can be found in dropdown.less -->
                 <li>
 
