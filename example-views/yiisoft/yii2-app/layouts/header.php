406da27173561ad29b40a102771665d8c80229b6
leonardojulius <leonardojulius.jcl@gmail.com>
2016-07-14 17:04:44 +0800

    remove app->name
    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' .
    Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>

--- example-views/yiisoft/yii2-app/layouts/header.php
+++ example-views/yiisoft/yii2-app/layouts/header.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 
 <header class="main-header">
 
-    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <?php //Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation">
 
