57030a600a031fec3653c689e6daa6d8a4fa4de1
leonardojulius <leonardojulius.jcl@gmail.com>
2016-07-16 03:16:51 +0800

    head
    hhead

--- example-views/yiisoft/yii2-app/layouts/header.php
+++ example-views/yiisoft/yii2-app/layouts/header.php
@@ -21,14 +21,14 @@ use yii\bootstrap\NavBar;
 if (Yii::$app->user->isGuest) {
     $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
     $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    $menuItems[] = ['label' => ''];
+    echo ' ';
 } else {
     $menuItems[] = [
         'label' => 'Logout ('. ' ' .Html::img('http://graph.facebook.com/'.Yii::$app->user->identity->profilepic.'/picture', ['class'=>'img-circle']) .' '. Yii::$app->user->identity->username . ')',
         'url' => ['/site/logout'],
         'linkOptions' => ['data-method' => 'post']
     ];
-    $menuItems[] = ['label' => ''];
+
 }
 
 echo Nav::widget([
