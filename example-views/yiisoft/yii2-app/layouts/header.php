598a91a0a1bf34d0ee36687113a226dd5f007df2
leonardojulius <leonardojulius.jcl@gmail.com>
2016-07-15 12:51:24 +0800

    Commit Notification
    Notifcation

--- example-views/yiisoft/yii2-app/layouts/header.php
+++ example-views/yiisoft/yii2-app/layouts/header.php
@@ -104,10 +104,10 @@ use yii\helpers\Html;
                 <li class="dropdown notifications-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="fa fa-bell-o"></i>
-                        <span class="label label-warning">10</span>
+                        <span class="label label-warning">1</span>
                     </a>
                     <ul class="dropdown-menu">
-                        <li class="header">You have 10 notifications</li>
+                        <li class="header">You have 1 notifications</li>
                         <li>
                             <!-- inner menu: contains the actual data -->
                             <ul class="menu">
