7dfbe1b9f53290b033d003ecd288c6b593ed1192
leonardojulius <leonardojulius.jcl@gmail.com>
2016-07-16 20:52:06 +0800

    h
    h

--- example-views/yiisoft/yii2-app/layouts/header.php
+++ example-views/yiisoft/yii2-app/layouts/header.php
@@ -24,8 +24,35 @@ if (Yii::$app->user->isGuest) {
 } else {
     $menuItems[] = [
         'label' => 'Logout ('. ' ' .Html::img('http://graph.facebook.com/'.Yii::$app->user->identity->profilepic.'/picture', ['class'=>'img-circle']) .' '. Yii::$app->user->identity->username . ')',
-        'url' => ['/site/logout'],
+       // 'url' => ['/site/logout'],
+        'items' =>'<li class="dropdown messages-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="fa fa-envelope-o"></i>
+                        <span class="label label-success">4</span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="header">You have 4 messages</li>
+                        <li>
+                            <!-- inner menu: contains the actual data -->
+                            <ul class="menu">
+                                <li><!-- start message -->
+                                    <a href="#">
+                                        <div class="pull-left">
+                                            <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle"
+                                                 alt="User Image"/>
+                                        </div>
+                                        <h4>
+                                            Support Team
+                                            <small><i class="fa fa-clock-o"></i> 5 mins</small>
+                                        </h4>
+                                        <p>Why not buy a new awesome theme?</p>
+                                    </a>
+                                </li>
+                                <!-- end message -->
+                                <li>
+        ',
         'linkOptions' => ['data-method' => 'post']
+
     ];
 
 }
