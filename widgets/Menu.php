eb90865596a52f5c0ad0f5f7000d2d24f3523583
githubjeka <et.coder@gmail.com>
2017-07-28 10:17:12 +0300

    Added info about prefix icon. Refactoring code for label of menu.

--- widgets/Menu.php
+++ widgets/Menu.php
@@ -14,17 +14,24 @@ class Menu extends \yii\widgets\Menu
      * @inheritdoc
      */
     public $linkTemplate = '<a href="{url}">{icon} {label}</a>';
+    /**
+     * @inheritdoc
+     * Styles all labels of items on sidebar by AdminLTE
+     */
+    public $labelTemplate = '<span>{label}</span>';
     public $submenuTemplate = "\n<ul class='treeview-menu' {show}>\n{items}\n</ul>\n";
     public $activateParents = true;
     public $defaultIconHtml = '<i class="fa fa-circle-o"></i> ';
-  
+
     /**
-     * @var string
+     * @var string is prefix that will be added to $item['icon'] if it exist.
+     * By default uses for Font Awesome (http://fontawesome.io/)
      */
     public static $iconClassPrefix = 'fa fa-';
 
     private $noDefaultAction;
     private $noDefaultRoute;
+
     /**
      * Renders the menu.
      */
@@ -56,45 +63,49 @@ class Menu extends \yii\widgets\Menu
             echo Html::tag($tag, $this->renderItems($items), $options);
         }
     }
-  
+
     /**
      * @inheritdoc
      */
     protected function renderItem($item)
     {
-        if(isset($item['items'])) {
+        if (isset($item['items'])) {
             $labelTemplate = '<a href="{url}">{icon} {label} <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>';
             $linkTemplate = '<a href="{url}">{icon} {label} <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span></a>';
-        }
-        else {
+        } else {
             $labelTemplate = $this->labelTemplate;
             $linkTemplate = $this->linkTemplate;
         }
 
         if (isset($item['url'])) {
             $template = ArrayHelper::getValue($item, 'template', $linkTemplate);
-            $replace = !empty($item['icon']) ? [
-                '{url}' => Url::to($item['url']),
-                '{label}' => '<span>'.$item['label'].'</span>',
-                '{icon}' => '<i class="' . self::$iconClassPrefix . $item['icon'] . '"></i> '
-            ] : [
-                '{url}' => Url::to($item['url']),
-                '{label}' => '<span>'.$item['label'].'</span>',
-                '{icon}' => $this->defaultIconHtml,
-            ];
-            return strtr($template, $replace);
+            $replace = !empty($item['icon'])
+                ? [
+                    '{url}' => Url::to($item['url']),
+                    '{label}' => $item['label'],
+                    '{icon}' => '<i class="' . self::$iconClassPrefix . $item['icon'] . '"></i> ',
+                ]
+                : [
+                    '{url}' => Url::to($item['url']),
+                    '{label}' => $item['label'],
+                    '{icon}' => $this->defaultIconHtml,
+                ];
         } else {
             $template = ArrayHelper::getValue($item, 'template', $labelTemplate);
-            $replace = !empty($item['icon']) ? [
-                '{label}' => '<span>'.$item['label'].'</span>',
-                '{icon}' => '<i class="' . self::$iconClassPrefix . $item['icon'] . '"></i> '
-            ] : [
-                '{label}' => '<span>'.$item['label'].'</span>',
-                '{icon}' => $this->defaultIconHtml
-            ];
-            return strtr($template, $replace);
+            $replace = !empty($item['icon'])
+                ? [
+                    '{label}' => $item['label'],
+                    '{icon}' => '<i class="' . self::$iconClassPrefix . $item['icon'] . '"></i> ',
+                ]
+                : [
+                    '{label}' => $item['label'],
+                    '{icon}' => $this->defaultIconHtml,
+                ];
         }
+
+        return strtr($template, $replace);
     }
+
     /**
      * Recursively renders the menu items (without the container tag).
      * @param array $items the menu items to be rendered recursively
@@ -135,6 +146,7 @@ class Menu extends \yii\widgets\Menu
         }
         return implode("\n", $lines);
     }
+
     /**
      * @inheritdoc
      */
@@ -174,6 +186,7 @@ class Menu extends \yii\widgets\Menu
         }
         return array_values($items);
     }
+
     /**
      * Checks whether a menu item is active.
      * This is done by checking if [[route]] and [[params]] match that specified in the `url` option of the menu item.
@@ -191,7 +204,7 @@ class Menu extends \yii\widgets\Menu
             if ($route[0] !== '/' && Yii::$app->controller) {
                 $route = ltrim(Yii::$app->controller->module->getUniqueId() . '/' . $route, '/');
             }
-            $route = ltrim($route,'/');
+            $route = ltrim($route, '/');
             if ($route != $this->route && $route !== $this->noDefaultRoute && $route !== $this->noDefaultAction) {
                 return false;
             }
