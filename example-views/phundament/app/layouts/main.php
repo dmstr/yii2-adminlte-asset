653cf20aef352a56d38303e6c2197a851cb15f6f
Tobias Munk <schmunk@usrbin.de>
2017-10-12 08:10:53 +0200

    fixed toggle menu

--- example-views/phundament/app/layouts/main.php
+++ example-views/phundament/app/layouts/main.php
@@ -40,7 +40,7 @@ dmstr\web\AdminLteAsset::register($this);
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top" role="navigation">
             <!-- Sidebar toggle button-->
-            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
                 <span class="sr-only">Toggle navigation</span>
             </a>
 
--- example-views/yiisoft/yii2-app/layouts/header.php
+++ example-views/yiisoft/yii2-app/layouts/header.php
@@ -11,7 +11,7 @@ use yii\helpers\Html;
 
     <nav class="navbar navbar-static-top" role="navigation">
 
-        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
             <span class="sr-only">Toggle navigation</span>
         </a>
 
