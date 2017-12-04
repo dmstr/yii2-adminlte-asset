dd4929bcfd1fcc764199b1728f169d14e2137e27
Tobias Munk <schmunk@usrbin.de>
2017-12-04 14:54:11 +0100

    added expand & collapse testing view

---
+++ example-views/testing/app/default/_expand-collapse.php
@@ -0,0 +1,79 @@
+<div class="row">
+    <div class="col-md-3">
+        <div class="box box-default collapsed-box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Expandable</h3>
+
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                    </button>
+                </div>
+                <!-- /.box-tools -->
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body" style="display: none;">
+                The body of the box
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-3">
+        <div class="box box-success">
+            <div class="box-header with-border">
+                <h3 class="box-title">Removable</h3>
+
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                </div>
+                <!-- /.box-tools -->
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                The body of the box
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-3">
+        <div class="box box-warning">
+            <div class="box-header with-border">
+                <h3 class="box-title">Collapsable</h3>
+
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                </div>
+                <!-- /.box-tools -->
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                The body of the box
+            </div>
+            <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-3">
+        <div class="box box-danger">
+            <div class="box-header">
+                <h3 class="box-title">Loading state</h3>
+            </div>
+            <div class="box-body">
+                The body of the box
+            </div>
+            <!-- /.box-body -->
+            <!-- Loading (remove the following to stop the loading)-->
+            <div class="overlay">
+                <i class="fa fa-refresh fa-spin"></i>
+            </div>
+            <!-- end loading -->
+        </div>
+        <!-- /.box -->
+    </div>
+    <!-- /.col -->
+</div>
--- example-views/testing/app/default/index.php
+++ example-views/testing/app/default/index.php
@@ -4,7 +4,7 @@
         <div class="small-box bg-blue">
             <div class="inner">
                 <h3>
-                     <?= YII_ENV ?>
+                    <?= YII_ENV ?>
                 </h3>
 
                 <p>
@@ -22,72 +22,72 @@
     <!-- ./col -->
 
 
-        <div class="col-md-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-aqua">
-                <div class="inner">
-                    <h3>
-                        n/a
-                    </h3>
+    <div class="col-md-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-aqua">
+            <div class="inner">
+                <h3>
+                    n/a
+                </h3>
 
-                    <p>
-                        Users
-                    </p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-person"></i>
-                </div>
-                <a href="<?= \yii\helpers\Url::to(['/user/admin']) ?>" class="small-box-footer">
-                    Manage <i class="fa fa-arrow-circle-right"></i>
-                </a>
+                <p>
+                    Users
+                </p>
             </div>
-        </div>
-        <!-- ./col -->
-
-        <div class="col-md-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-orange">
-                <div class="inner">
-                    <h3>
-                        <?= count(\Yii::$app->getModules()) ?>
-                    </h3>
-
-                    <p>
-                        Modules
-                    </p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-stats-bars"></i>
-                </div>
-                <a href="<?= \yii\helpers\Url::to(['/debug']) ?>" class="small-box-footer">
-                    Debug <i class="fa fa-arrow-circle-right"></i>
-                </a>
+            <div class="icon">
+                <i class="ion ion-person"></i>
             </div>
-
+            <a href="<?= \yii\helpers\Url::to(['/user/admin']) ?>" class="small-box-footer">
+                Manage <i class="fa fa-arrow-circle-right"></i>
+            </a>
         </div>
-        <!-- ./col -->
+    </div>
+    <!-- ./col -->
 
-        <div class="col-md-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-red">
-                <div class="inner">
-                    <h3>
-                        <?= getenv('APP_VERSION') ?>
-                    </h3>
+    <div class="col-md-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-orange">
+            <div class="inner">
+                <h3>
+                    <?= count(\Yii::$app->getModules()) ?>
+                </h3>
 
-                    <p>
-                        Version
-                    </p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-grid"></i>
-                </div>
-                <a href="<?= \yii\helpers\Url::to('http://phundament.com') ?>" target="_blank" class="small-box-footer">
-                    Phundament Online <i class="fa fa-arrow-circle-right"></i>
-                </a>
+                <p>
+                    Modules
+                </p>
             </div>
+            <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="<?= \yii\helpers\Url::to(['/debug']) ?>" class="small-box-footer">
+                Debug <i class="fa fa-arrow-circle-right"></i>
+            </a>
         </div>
-        <!-- ./col -->
+
+    </div>
+    <!-- ./col -->
+
+    <div class="col-md-3 col-xs-6">
+        <!-- small box -->
+        <div class="small-box bg-red">
+            <div class="inner">
+                <h3>
+                    <?= getenv('APP_VERSION') ?>
+                </h3>
+
+                <p>
+                    Version
+                </p>
+            </div>
+            <div class="icon">
+                <i class="ion ion-grid"></i>
+            </div>
+            <a href="<?= \yii\helpers\Url::to('http://phundament.com') ?>" target="_blank" class="small-box-footer">
+                Phundament Online <i class="fa fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div>
+    <!-- ./col -->
 
 </div>
 
@@ -113,56 +113,57 @@
 </div>
 
 
-
-    <div class="row">
-        <div class="col-sm-6">
-            <!-- Default box -->
-            <div class="box">
-                <div class="box-header">
-                    <h3 class="box-title">Modules</h3>
-                </div>
-                <div class="box-body">
-                    <?php
-                    foreach (\Yii::$app->getModules() AS $name => $m) {
-                        $module = \Yii::$app->getModule($name);
-                        echo yii\helpers\Html::a(
-                            $module->id,
-                            ['/' . $module->id],
-                            ['class' => 'btn btn-default btn-flat']
-                        );
-                    }
-                    ?>
-                </div>
-                <!-- /.box-body -->
-                <div class="box-footer">
-                    <small>Registered in application from configuration or bootstrapping.</small>
-                </div>
-                <!-- /.box-footer-->
+<div class="row">
+    <div class="col-sm-6">
+        <!-- Default box -->
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title">Modules</h3>
             </div>
-            <!-- /.box -->
-        </div>
-
-        <div class="col-sm-6">
-            <!-- Default box -->
-            <div class="box">
-                <div class="box-header">
-                    <h3 class="box-title">Documentation</h3>
-                </div>
-                <div class="box-body">
-                    <div class="alert alert-info">
-                        <i class="fa fa-warning"></i>
-                        <b>Notice!</b> Use the <i>yii2-apidoc</i> extension to
-                        create the HTML documentation for this application.
-                    </div>
-                </div>
-                <!-- /.box-body -->
-                <div class="box-footer">
-                    
-                </div>
-                <!-- /.box-footer-->
+            <div class="box-body">
+                <?php
+                foreach (\Yii::$app->getModules() AS $name => $m) {
+                    $module = \Yii::$app->getModule($name);
+                    echo yii\helpers\Html::a(
+                        $module->id,
+                        ['/'.$module->id],
+                        ['class' => 'btn btn-default btn-flat']
+                    );
+                }
+                ?>
             </div>
-            <!-- /.box -->
+            <!-- /.box-body -->
+            <div class="box-footer">
+                <small>Registered in application from configuration or bootstrapping.</small>
+            </div>
+            <!-- /.box-footer-->
         </div>
+        <!-- /.box -->
     </div>
+
+    <div class="col-sm-6">
+        <!-- Default box -->
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title">Documentation</h3>
+            </div>
+            <div class="box-body">
+                <div class="alert alert-info">
+                    <i class="fa fa-warning"></i>
+                    <b>Notice!</b> Use the <i>yii2-apidoc</i> extension to
+                    create the HTML documentation for this application.
+                </div>
+            </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
+
+            </div>
+            <!-- /.box-footer-->
+        </div>
+        <!-- /.box -->
+    </div>
+</div>
+
+<?= $this->render('_expand-collapse') ?>
 
 
